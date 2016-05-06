4c1f28058e2b67394b6a440365d1f07742ca3eab
iansun <lauer3912@gmail.com>
2016-05-06 11:11:21 +0800

    Update

---
+++ js/config.js
@@ -0,0 +1,15 @@
+(function () {
+    window.RTYConfig = {
+        appName:"FastDownloader",
+        appID:"com.romanysoft.macos.app.milu.fastdownloader",
+        documentTitle:"FastDownloader By Romanysoft",
+        supportPlatforms:["Mac"],
+        googleUA:"UA-43238484-5",
+        gitHome:"//github.com/Romanysoft/FastDownloader",
+        reportIssueUrl:"//github.com/Romanysoft/FastDownloader/issues",
+        changeLogUrl:"//github.com/Romanysoft/FastDownloader/wiki/Changelog",
+        wikiUrl:"//github.com/Romanysoft/FastDownloader/wiki",
+        romanysoftHome:"//www.romanysoft.com",
+        copyright:"Copyright © " + (new Date()).getFullYear() + " Romanysoft LAB."
+    }
+})();
